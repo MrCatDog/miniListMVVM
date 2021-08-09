e5b4f0d37d3859e4ab0067d1312f67d11b9236a2
MrCatDog <failoff.catdog@yandex.ru>
2021-08-10 00:53:42 +0300

    Settings presenter on work

--- app/src/main/java/com/example/minilist/SettingsActivity.java
+++ app/src/main/java/com/example/minilist/SettingsActivity.java
@@ -8,6 +8,9 @@ import com.example.minilist.databinding.ActivitySettingsBinding;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    public final static String SETTINGS_TEXT_COLOR = "text_color";
+    public final static String SETTINGS_TEXT_SIZE = "text_size";
+
     private SettingsPresenter presenter;
     private ActivitySettingsBinding binding;
 
@@ -18,5 +21,11 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         presenter = new SettingsPresenter(this);
+
+        binding.radioTextSize.setOnCheckedChangeListener((view, check) -> presenter.changeSize(view));
+        binding.radioTextColor.setOnCheckedChangeListener((view, check) -> presenter.changeColor(view));
+
+        binding.radio16.toggle();
+        binding.radioBlack.toggle();
     }
 }
--- app/src/main/java/com/example/minilist/SettingsPresenter.java
+++ app/src/main/java/com/example/minilist/SettingsPresenter.java
@@ -1,10 +1,50 @@
 package com.example.minilist;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.RadioGroup;
+
 public class SettingsPresenter {
 
     private final SettingsActivity wireframe;
 
     SettingsPresenter(SettingsActivity wireframe) {
         this.wireframe = wireframe;
+    }
+
+    public void changeSize(RadioGroup radioGroup) {
+        SharedPreferences.Editor save = PreferenceManager.getDefaultSharedPreferences(wireframe).edit();
+        int size;
+        switch (radioGroup.getCheckedRadioButtonId()) {
+            case R.id.radio_14:
+                size = R.dimen.little_text_size;
+                break;
+            case R.id.radio_18:
+                size = R.dimen.big_text_size;
+                break;
+            default:
+                size = R.dimen.average_text_size;
+                break;
+        }
+        save.putInt(SettingsActivity.SETTINGS_TEXT_SIZE, size);
+        save.apply();
+    }
+
+    public void changeColor(RadioGroup radioGroup) {
+        SharedPreferences.Editor save = PreferenceManager.getDefaultSharedPreferences(wireframe).edit();
+        int color;
+        switch (radioGroup.getCheckedRadioButtonId()) {
+            case R.id.radio_burnt_umber:
+                color = R.color.burnt_umber;
+                break;
+            case R.id.radio_kashmir_green:
+                color = R.color.kashmir_green;
+                break;
+            default:
+                color = R.color.black;
+                break;
+        }
+        save.putInt(SettingsActivity.SETTINGS_TEXT_COLOR, color);
+        save.apply();
     }
 }
