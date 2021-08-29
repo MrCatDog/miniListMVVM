19cbb7dd08ea553b32f6750830321d26ce3a41c3
MrCatDog <failoff.catdog@yandex.ru>
2021-08-29 18:53:02 +0300

    All my homies said NO to premature optimization

--- app/src/main/java/com/example/minilist/EditActivityPresenter.java
+++ app/src/main/java/com/example/minilist/EditActivityPresenter.java
@@ -1,11 +1,11 @@
 package com.example.minilist;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 import static com.example.minilist.MainPresenter.FILE_NAME;
 
@@ -13,26 +13,15 @@ public class EditActivityPresenter {
 
     private final EditActivity wireframe;
 
-    private String text;
-
     EditActivityPresenter(EditActivity wireframe) {
         this.wireframe = wireframe;
         File source = new File(wireframe.getFilesDir(), FILE_NAME);
         if (source.exists()) {
-            try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-                while ((line = in.readLine()) != null) {
-                    stringBuilder.append(line).append("\n");
-                }
-                text = stringBuilder.toString();
-                this.wireframe.setText(text);
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-                //откинуть снэк о невозможности чтения, СТОП, а это возможно в этом случае? хотя почему бы и нет
+            try (Scanner in = new Scanner(source)) {
+                this.wireframe.setText(in.useDelimiter("\\Z").next());
+            } catch (FileNotFoundException exception) {
+                exception.printStackTrace();
             }
-        } else {
-            text = "";
         }
     }
 
@@ -40,17 +29,13 @@ public class EditActivityPresenter {
         File file = new File(wireframe.getFilesDir(), FILE_NAME);
         if (text.isEmpty()) {
             //del
-            if (file.exists()) {
-                file.delete();
-            }
+            file.delete();
         } else {
             //save
-            if (!text.equals(this.text)) {
-                try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
-                    out.write(text);
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
-                }
+            try (BufferedWriter out = new BufferedWriter(new FileWriter(file))) {
+                out.write(text);
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
             }
         }
     }
--- app/src/main/java/com/example/minilist/MainPresenter.java
+++ app/src/main/java/com/example/minilist/MainPresenter.java
@@ -8,10 +8,7 @@ public class MainPresenter {
 
     public final static String FILE_NAME = "miniList";
 
-    private enum State {EXIST, NOT_EXIST}
-
     private final MainActivity wireframe;
-    private State status;
 
     public MainPresenter(MainActivity wireframe) {
         this.wireframe = wireframe;
@@ -20,23 +17,9 @@ public class MainPresenter {
 
     public void checkFileExists() {
         if (new File(wireframe.getFilesDir(), FILE_NAME).exists()) {
-            fileExist();
-        } else {
-            newFile();
-        }
-    }
-
-    public void fileExist() {
-        if (status != State.EXIST) {
             wireframe.setExist();
-            status = State.EXIST;
-        }
-    }
-
-    public void newFile() {
-        if (status != State.NOT_EXIST) {
+        } else {
             wireframe.setNew();
-            status = State.NOT_EXIST;
         }
     }
 
