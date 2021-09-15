4f648b27230d4f3d666db74cd70ceb80ea46243e
MrCatDog <failoff.catdog@yandex.ru>
2021-09-15 21:58:56 +0300

    Shared.kt is an object now

--- app/src/main/java/com/example/minilist/Shared.kt
+++ app/src/main/java/com/example/minilist/Shared.kt
@@ -1,15 +1,13 @@
 package com.example.minilist
 
-class Shared {
-    companion object {
-        const val FILE_NAME = "miniList"
-        const val SETTINGS_TEXT_COLOR = "text_color"
-        const val SETTINGS_TEXT_SIZE = "text_size"
-        const val LITTLE_SIZE = 0
-        const val AVERAGE_SIZE = 1
-        const val BIG_SIZE = 2
-        const val BLACK_COLOR = 0
-        const val UMBER_COLOR = 1
-        const val GREEN_COLOR = 2
-    }
+object Shared {
+    const val FILE_NAME = "miniList"
+    const val SETTINGS_TEXT_COLOR = "text_color"
+    const val SETTINGS_TEXT_SIZE = "text_size"
+    const val LITTLE_SIZE = 0
+    const val AVERAGE_SIZE = 1
+    const val BIG_SIZE = 2
+    const val BLACK_COLOR = 0
+    const val UMBER_COLOR = 1
+    const val GREEN_COLOR = 2
 }
